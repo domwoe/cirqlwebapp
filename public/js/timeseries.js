47dca983cd5d5e993ea1720c7c8afab6be95f29c
Wörner  Dominic <dwoerner@ethz.ch>
2013-10-11 16:18:37 +0200

    Tidied up

--- public/js/timeseries.js
+++ public/js/timeseries.js
@@ -1,19 +1,41 @@
 $(function() {
-    console.log('Hello world');
+
+    Highcharts.setOptions({
+        global: {
+            useUTC: false
+        }
+    });
 
     chartOptions = {
+
         chart : {
-            // type: '',
+            type: 'area',
             zoomType: 'x',
             renderTo: 'container'
         },
 
-        // navigator : {
-        //     adaptToUpdatedData: false,
-        //     series : {
-        //         data : data
-        //     }
-        // },
+        plotOptions: {
+            area: {
+                fillColor: {
+                    linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
+                    stops: [
+                        [0, Highcharts.getOptions().colors[0]],
+                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                    ]
+                },
+                lineWidth: 1,
+                marker: {
+                    enabled: false
+                },
+                shadow: false,
+                states: {
+                    hover: {
+                        lineWidth: 1
+                    }
+                },
+                threshold: null
+            }
+        },
 
         scrollbar: {
             liveRedraw: false
@@ -31,60 +53,40 @@ $(function() {
         //     buttons: [{
         //         type: 'hour',
         //         count: 1,
-        //         text: '1h'
+        //         text: 'Stunde'
         //     }, {
         //         type: 'day',
         //         count: 1,
-        //         text: '1d'
+        //         text: 'Tag'
         //     }, {
-        //         type: 'month',
+        //         type: 'week',
         //         count: 1,
-        //         text: '1m'
-        //     }, {
-        //         type: 'year',
-        //         count: 1,
-        //         text: '1y'
-        //     }, {
-        //         type: 'all',
-        //         text: 'All'
+        //         text: 'Woche'
         //     }],
         //     inputEnabled: false, // it supports only days
-        //     selected : 4 // all
+        //     selected : 3 // week
         // },
         
         xAxis : {
             type: 'datetime'
-            // events : {
-            //     afterSetExtremes : afterSetExtremes
-            // },
-            // minRange: 3600 * 1000 // one hour
         },
 
         series : [{
-            //data : []
-            //dataGrouping: {
-            //    enabled: false
-            //}
-        }]
+        }],
+
+        legend: {   
+            enabled: false
+        },
+        
+        credits: {
+            enabled: false
+        }
     };    
     
     $(document).ready(function() {
-        $.getJSON('http:///213.165.92.187:8080/api/user/1/room/14/quantity=temperature?callback=?', function(data) {
+        $.getJSON('http:///213.165.92.187:8080/api/user/1/room/14/quantity=co2?callback=?', function(data) {
      
-        // $.ajax({
-        //     url : 'http:///213.165.92.187:8080/api/user/1/room/14/quantity=temperature?callback=?',
-        //     type : 'GET',
-        //     jsonpCallback: 'jsonCallback',
-        //     contentType: "application/json",
-        //     dataType : 'jsonp',
-        //     //1jsonp : 'callback',
-        //     success : function(data){    
-            
-                console.log(data[0].timestamp); 
-
-                // Add a null value for the end date 
-                //data = [].concat(data, [[Date.UTC(2011, 9, 14, 19, 59), null, null, null, null]]);
-                    
+                
                 // create the chart
                 chart = new Highcharts.Chart(chartOptions);
                 $.each(data, function(i,point) {
@@ -97,22 +99,3 @@ $(function() {
     });    
 });
 
-
-/**
- * Load new data depending on the selected min and max
- */
-function afterSetExtremes(e) {
-
-    var url,
-        currentExtremes = this.getExtremes(),
-        range = e.max - e.min;
-    var chart = $('#container').highcharts();
-    chart.showLoading('Loading data from server...');
-    $.getJSON('http://www.highcharts.com/samples/data/from-sql.php?start='+ Math.round(e.min) +
-            '&end='+ Math.round(e.max) +'&callback=?', function(data) {
-        
-        chart.series[0].setData(data);
-        chart.hideLoading();
-    });
-    
-}
