7e7eb7ef7da748987b5f6972159f991d42c31715
Wörner  Dominic <dwoerner@ethz.ch>
2013-10-14 20:37:43 +0200

    Added colors and yAxis title according to quantity

--- public/js/timeseries.js
+++ public/js/timeseries.js
@@ -1,5 +1,29 @@
 $(function() {
 
+    $.urlParam = function(name){
+            var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
+            if (results==null){
+                return null;
+            }else{
+                return results[1] || 0;
+            }   
+        }
+    var user = $.urlParam('user');
+    var room = $.urlParam('room');
+    var quantity = $.urlParam('quantity');
+    var period = $.urlParam('period');
+
+    // Set colors for area according to quantitiy 
+    if (quantity === 'temperature') {
+        rgbcolor = '#f39c12';
+    }   
+    else if (quantity === 'humidity') {
+        rgbcolor = '#3498db'
+    }
+    else if (quantity === 'co2') {
+        rgbcolor = '#2ecc71'
+    } 
+
     Highcharts.setOptions({
         global: {
             useUTC: false
@@ -19,8 +43,8 @@ $(function() {
                 fillColor: {
                     linearGradient: { x1: 0, y1: 0, x2: 0, y2: 1},
                     stops: [
-                        [0, Highcharts.getOptions().colors[0]],
-                        [1, Highcharts.Color(Highcharts.getOptions().colors[0]).setOpacity(0).get('rgba')]
+                        [0, rgbcolor],
+                        [1, Highcharts.Color(rgbcolor).setOpacity(0).get('rgba')]
                     ]
                 },
                 lineWidth: 1,
@@ -84,21 +108,26 @@ $(function() {
     }; 
 
     chart = new Highcharts.Chart(chartOptions);
-                chart.showLoading();   
+                chart.showLoading();
+
+    if (quantity === 'temperature') {
+        chart.yAxis[0].axisTitle.attr({
+            text: 'Temperatur [°C]'
+        });
+    }   
+    else if (quantity === 'humidity') {
+        chart.yAxis[0].axisTitle.attr({
+            text: 'Luftfeuchtigkeit [%]'
+        });
+    }
+    else if (quantity === 'co2') {
+        chart.yAxis[0].axisTitle.attr({
+            text: 'Luftqualität (CO2 [ppm])'
+        });
+    } 
     
     $(document).ready(function() {
-        $.urlParam = function(name){
-            var results = new RegExp('[\\?&]' + name + '=([^&#]*)').exec(window.location.href);
-            if (results==null){
-                return null;
-            }else{
-                return results[1] || 0;
-            }   
-        }
-        var user = $.urlParam('user');
-        var room = $.urlParam('room');
-        var quantity = $.urlParam('quantity');
-        var period = $.urlParam('period');
+        
         $.getJSON('http:///213.165.92.187:8080/api/user/'+user+'/room/'+room+'/quantity='+quantity+'&period='+period+'?callback=?', function(data) {
      
                 
@@ -109,6 +138,7 @@ $(function() {
                     point.y = point.value;
                 }); 
                 chart.series[0].setData(data);
+                
             
         });
     });    
